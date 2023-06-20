08b149490c5d7c913dffe60b7885ce5d70ffdc24
Cathnava21 <131034603+Cathnava21@users.noreply.github.com>
2023-06-20 23:39:14 +0000

    new monster

---
+++ MinotaurLabyrinth/Monsters/GelatinousCube.cs
@@ -0,0 +1,42 @@
+﻿namespace MinotaurLabyrinth
+{
+    /// <summary>
+    /// Represents a Slimy gel monster in the game.
+    /// </summary>
+    public class GelatinousCube : Monster, IMoveable
+    {
+        private Location _location;
+
+        public GelatinousCube (Location location){
+            _location = location;
+            
+        }
+
+        public Location getLocation() {
+            return _location;
+        }
+        public override void Activate(Hero hero, Map map)
+        {
+            ConsoleHelper.WriteLine("You have encountered the minotaur! He charges at you and knocks you into another room.", ConsoleColor.Magenta);
+        }
+
+
+        public override bool DisplaySense(Hero hero, int heroDistance)
+        {
+            return false;
+        }
+
+
+        public override DisplayDetails Display()
+        {
+            return new DisplayDetails("[C]", ConsoleColor.Green);
+        }
+
+        public void Move(Hero hero, Map map)
+        {   
+            map.GetRoomAtLocation(_location).RemoveMonster();
+            _location = new Location (_location.Row +1, _location.Column);
+            map.GetRoomAtLocation(_location).AddMonster(this);
+        }
+    }
+}
--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -4,11 +4,77 @@ namespace MinotaurLabyrinthTest
 {
     [TestClass]
     public class Tests
-    {
+    {   
+        /*
         [TestMethod]
-        public void DummyTest()
+        public void PitRoomTest()
         {
-            Assert.AreNotSame(1, 2);
+            //Seed the RandomNumberGenerator 
+            RandomNumberGenerator.SetSeed(1);
+            
+            Pit PitRoom =new Pit();
+            Hero hero = new Hero();
+            Map map = new Map (1,1);
+
+            PitRoom.Activate (hero,map);
+            Assert.AreEqual (PitRoom.IsActivate,false);
+            Assert.AreEqual (hero.IsAlive,true);
+
+            hero.HasSword = true;
+            PitRoom.Activate (hero,map);
+            Assert.AreEqual(hero.IsAlive,true);
+
+            Pit newPitRoom = new Pit();
+            newPitRoom.Activate(hero,map);
+            Assert.AreEqual(hero.IsAlive,true);
+
+            newPitRoom.Activate (hero,map);
+            newPitRoom= new Pit();
+            newPitRoom.Activate (hero,map);
+            newPitRoom= new Pit();
+            newPitRoom.Activate (hero,map);
+            Assert.AreEqual(hero.IsAlive,false);
+        }
+        [TestMethod]
+        public void MinotaurTest()
+        {
+            Hero hero = new Hero();
+            Minotaur minotaur = new Minotaur();
+            Map map = new Map(4,4);
+            hero.HasSword= true;
+            Assert.AreEqual(hero.HasSword,true);
+
+            minotaur.Activate(hero,map);
+            Assert.AreEqual(hero.Location, new Location(0,2));
+            Assert.AreEqual(hero.HasSword,false);
+            
+
+            minotaur.Activate(hero,map);
+            Assert.AreEqual(hero.Location,new Location(0,3));
+
+            hero.Location = new Location (3,1);
+            minotaur.Activate(hero,map);
+            Assert.AreEqual(hero.Location,new Location(2,3));
+        }
+        */
+        [TestMethod]
+
+        public void GelMoveTest()
+        {
+            Hero hero = new Hero();
+            var gelLocation = new Location (1,1);
+            GelatinousCube gel = new (gelLocation);
+            Map map = new Map(4,4);
+            map.GetRoomAtLocation(gelLocation).AddMonster(gel);
+            gel.Move(hero,map);
+            //New location should be (2,1)
+            var expectedLocation = new Location (2,1);
+            Assert.AreEqual(expectedLocation,gel.getLocation());
+            gel.Move(hero,map);
+            var expectedLocation = new Location (2,1);
+            Assert.AreEqual(expectedLocation,gel.getLocation());
+            var expectedLocation = new Location (2,1);
+            Assert.AreEqual(expectedLocation,gel.getLocation());
         }
     }
 }
